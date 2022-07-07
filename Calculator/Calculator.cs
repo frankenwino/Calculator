01a23ed3724a23dd72bec78da5eeacde480e4dd6
Andrew Browne <3808083+frankenwino@users.noreply.github.com>
2022-07-07 15:08:04 +0200

    Initial commit.

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+namespace Calculator
+{
+    public class Operation
+    {
+        public static string Add(decimal firstNumber, decimal secondNumber)
+        {
+            decimal result = firstNumber + secondNumber;
+            string resultString = $"{firstNumber} + {secondNumber} = {result}";
+
+            return resultString;
+        }
+
+        public static string Add(params decimal[] numbers)
+        {
+            decimal result = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                result += numbers[i];
+            }
+            string lhs = String.Join(" + ", numbers);
+            string resultString = $"{lhs} = {result}";
+
+            return resultString;
+        }
+
+        public static string Subtract(decimal firstNumber, decimal secondNumber)
+        {
+            decimal result = firstNumber - secondNumber;
+            string resultString = $"{firstNumber} - {secondNumber} = {result}";
+
+            return resultString;
+        }
+
+        public static string Subtract(params decimal[] numbers)
+        {
+            decimal result = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                result -= numbers[i];
+            }
+
+            string lhs = String.Join(" - ", numbers);
+            string resultString = $"{lhs} = {result}";
+
+            return resultString;    
+        }
+
+        public static string Multiply(decimal firstNumber, decimal secondNumber)
+        {
+            decimal result = firstNumber * secondNumber;
+            string resultString = $"{firstNumber} + {secondNumber} = {result}";
+
+            return resultString;
+        }
+
+        public static string Divide(decimal firstNumber, decimal secondNumber)
+        {
+            string resultString;
+            try
+            {
+                decimal result = firstNumber / secondNumber;
+                resultString = $"{firstNumber} / {secondNumber} = {result}";
+            }
+            catch (System.DivideByZeroException ex)
+            {
+                resultString = $"DivideByZeroException caught: {ex.Message}";
+            }
+
+
+            return resultString;
+
+        }
+
+    }
+}
+
---
+++ Calculator/Utilities.cs
@@ -0,0 +1,72 @@
+﻿namespace Calculator
+{
+    public class Utilities
+    {
+        public static void EnterToContinue()
+        {
+            Console.WriteLine("\nPress ENTER to continue");
+            Console.ReadLine();
+        }
+
+        public static decimal InputNumber(string inputQuestion= "Enter a number: ")
+        {
+            decimal number;
+            bool isdecimal;
+            do
+            {
+                Console.Write(inputQuestion);
+                string userInput = Console.ReadLine().Trim();
+                isdecimal = decimal.TryParse(userInput, out number);
+
+                if (!isdecimal)
+                {
+                    Console.WriteLine("Error! Numbers only!");
+                }
+
+            } while (!isdecimal);
+
+            return number;
+        }
+
+        public static decimal[] CreateDecimalArray()
+        {
+            List<decimal> numbersList = new();
+            string userInput = string.Empty;
+            bool continueAdding = true;
+
+            while (continueAdding)
+            {
+                Console.Write("Enter a number (or q to quit): ");
+                userInput = Console.ReadLine().Trim().ToLower();
+
+                if (userInput == "q" && numbersList.Count > 2)
+                {
+                    continueAdding = false;
+                }
+                else if (userInput == "q" && numbersList.Count < 3)
+                {
+                    Console.WriteLine($"Error! You have input only {numbersList.Count} number(s). Need at least three numbers.");
+                }
+                else
+                {
+                    decimal number;
+                    bool isdecimal = decimal.TryParse(userInput, out number);
+
+                    if (!isdecimal)
+                    {
+                        Console.WriteLine("Error! Numbers only!");
+                    }
+                    else
+                    {
+                        numbersList.Add(number);
+                    }
+                } 
+            }
+
+            decimal[] numbersdecimalArray = numbersList.ToArray();
+
+            return numbersdecimalArray;
+        }
+    }
+}
+
