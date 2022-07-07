c406c05373a1a76154a09651fac6c9745cfb3c32
Andrew Browne <3808083+frankenwino@users.noreply.github.com>
2022-07-07 15:52:02 +0200

    Initial Commit

---
+++ Calculator.Tests/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using Calculator;
+using Xunit;
+
+namespace Calculator.Tests;
+
+public class AddTest
+{
+    [Fact]
+    public void AddTwoNumbersTest()
+    {
+
+        //arrange
+        decimal firstNumber = 1;
+        decimal secondNumber = 1;
+
+        //act
+        string result = Operation.Add(firstNumber, secondNumber);
+
+
+        //assert
+        Assert.Equal("1 + 1 = 2", result);
+    }
+
+    [Fact]
+    public void AddArrayOfNumbersTest()
+    {
+
+        //arrange
+        decimal[] numbers = { 1, 1, 1 };
+
+        //act
+        string result = Operation.Add(numbers);
+
+
+        //assert
+        Assert.Equal("1 + 1 + 1 = 3", result);
+    }
+}
