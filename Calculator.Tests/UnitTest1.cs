48b422fb6541e33c18417b853f855faff3f503a5
Andrew Browne <3808083+frankenwino@users.noreply.github.com>
2022-07-07 16:05:49 +0200

    Created subtraction, multiplication and division tests.

--- Calculator.Tests/UnitTest1.cs
+++ Calculator.Tests/UnitTest1.cs
@@ -16,7 +16,6 @@ public class AddTest
         //act
         string result = Operation.Add(firstNumber, secondNumber);
 
-
         //assert
         Assert.Equal("1 + 1 = 2", result);
     }
@@ -31,8 +30,87 @@ public class AddTest
         //act
         string result = Operation.Add(numbers);
 
-
         //assert
         Assert.Equal("1 + 1 + 1 = 3", result);
+    }
+}
+
+public class SubtractTest
+{
+    [Fact]
+    public void SubtractOneFromOtherTest()
+    {
+        //arrange
+        decimal firstNumber = 2;
+        decimal secondNumber = 3;
+
+        //act
+        string result = Operation.Subtract(firstNumber, secondNumber);
+
+        //assert
+        Assert.Equal("2 - 3 = -1", result);
+    }
+
+    [Fact]
+    public void SubtractArrayOfNumbersTest()
+    {
+
+        //arrange
+        decimal[] numbers = { 10, 3, 2, 1 };
+
+        //act
+        string result = Operation.Subtract(numbers);
+
+        //assert
+        Assert.Equal("10 - 3 - 2 - 1 = 4", result);
+    }
+}
+
+public class MultiplyTest
+{
+    [Fact]
+    public void MultiplyTwoNumbersTest()
+    {
+        //arrange
+        decimal firstNumber = 5;
+        decimal secondNumber = 5;
+
+        //act
+        string result = Operation.Multiply(firstNumber, secondNumber);
+
+        //assert
+        Assert.Equal("5 * 5 = 25", result);
+    }
+}
+
+
+public class DivideTest
+{
+    [Fact]
+    public void DivideTwoNumbersTest()
+    {
+        //arrange
+        decimal firstNumber = 10;
+        decimal secondNumber = 2;
+
+        //act
+        string result = Operation.Divide(firstNumber, secondNumber);
+
+        //assert
+        Assert.Equal("10 / 2 = 5", result);
+    }
+
+    [Fact]
+    public void DivideByZeroTest()
+    {
+        //arrange
+        decimal firstNumber = 10;
+        decimal secondNumber = 0;
+
+        //act
+        string result = Operation.Divide(firstNumber, secondNumber);
+
+        //assert
+        Assert.Equal("DivideByZeroException caught: Attempted to divide by zero.", result);
     }
 }
